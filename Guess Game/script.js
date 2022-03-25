c3e3f8f997e267c90ca1b85746bec2ae8fdf4199
Adetayo Boluwatife <Samuelchristiana38@gmail.com>
2022-03-25 11:47:51 +0100

    Add files via upload

---
+++ Guess Game/script.js
@@ -0,0 +1,15 @@
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+  function myFunction() {
+    let myGuess = guess.value
+    let text;
+    if (myGuess == randomNumber) {
+      text = "You got it right!";
+    } else if (myGuess > randomNumber) {
+      text = "Your guess was " + myGuess + ". That's too high. Try Again!";
+    } else if (myGuess < randomNumber) {
+      text = "Your guess was " + myGuess + ". That's too low. Try Again!";
+    }
+    document.getElementById("feedback").innerHTML = text;
+  }
+  
